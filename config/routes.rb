d54e1b9bff46f460bb3934953085e9402301ef01
Jeniffer Callou <jenifferprc@gmail.com>
2021-07-10 16:38:51 +0200

    Rails Task Manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # list all tasks
+  get '/tasks', to: 'tasks#index'
+
+  # add a new task
+  get '/tasks/new', to: 'tasks#new'
+  post '/tasks', to: 'tasks#create'
+
+  # view the details of a task
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
+  # edit a task
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch '/tasks/:id', to: 'tasks#update'
+
+  # remove a task
+  delete '/tasks/:id', to: 'tasks#destroy', as: :delete
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  task = Task.create(
+    title: Faker::ProgrammingLanguage.name,
+    details: Faker::ProgrammingLanguage.creator,
+    completed: false
+    )
+  puts "Task #{task.title} created!"
+end
